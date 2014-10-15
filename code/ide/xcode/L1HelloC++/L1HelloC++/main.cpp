3b592ceb886c05c0bb48d5ad20eaf5255a52e764
JingHaifeng <huq33@126.com>
2014-10-16 00:13:06 +0800

    add L1

---
+++ code/ide/xcode/L1HelloC++/L1HelloC++/main.cpp
@@ -0,0 +1,37 @@
+//
+//  main.cpp
+//  L1HelloC++
+//
+//  Created by Alex on 14-10-15.
+//  Copyright (c) 2014年 alex. All rights reserved.
+//
+
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+//class People{
+//public:
+//    void sayHello(){
+//        printf("hello C++ \n");
+//    }
+//};
+int main(int argc, const char * argv[])
+{
+
+//    1.3
+//    insert code here...
+//    People *p = new People();
+//    p->sayHello();
+//    free(p);
+//    delete(p);
+//    1.4
+    int v1 = 0,v2=0;
+    cout << "enter two number"<<endl;
+    cin >> v1 >> v2;
+    cout << v1 << " * " << v2 << " = " << v1*v2 << endl;
+    
+    return 0;
+}
+
